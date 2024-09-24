9bda959c5a0a49b85f094b65840ac099446efbff
KevinCirne1 <121322605+KevinCirne1@users.noreply.github.com>
2024-09-23 22:22:59 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from "react";  // Importa a biblioteca React
+import ReactDOM from "react-dom";  // Importa a biblioteca ReactDOM
+import App from "./App";  // Importa o componente principal App 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
+ReactDOM.render(  // Chama o método render da biblioteca ReactDOM, que renderiza um elemento React no DOM.
+  <React.StrictMode>  // Um wrapper que ativa verificações e avisos adicionais para os componentes
+    <App />  // vai renderizar o componente App dentro do StrictMode.
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root")  /componente App deve ser inserido no elemento com o ID "root".
 );
